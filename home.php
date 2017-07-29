5e38702d9bf2e5c84638d5875220c69856fe5e85
isha <ziyaan.verma@gmail.com>
2017-07-29 14:40:35 +0000

    added dynamic menus

--- home.php
+++ home.php
@@ -1,6 +1,7 @@
 <?php 
 
 session_start();
+include("includes/connection.php");
 
 ?>
 
@@ -8,10 +9,38 @@ session_start();
 <html>
 <head>
 	<title> Welcome user </title>
-	  <link rel="stylesheet" href="css/home_style.css">
+	  <link rel="stylesheet" href="css/home_style.css" media="all" />
 </head>
 
 <body>
+<div class="container">
+    <div id="head_wrap">
+        <div id="header">
+            <ul id="menu">
+                <li><a href="home.php">Home</a></li>
+                <li><a href="members.php">members</a></li>
+                <strong>Topics</strong>
+                <?php
+                
+                $get_topics="select * from topics";
+                $run_topics=mysqli_query($con,$get_topics);
+                
+                while($row=mysqli_fetch_array($run_topics)){
+                    
+                    //both the parameters in brackets are field name from  database
+                    $topic_id=$row['topic_id'];
+                    $topic_title=$row['topic_title'];
+                    
+                    //here we are show title  of the  topics from database
+                    echo "<li> <a href='home.php?$topic_id'>$topic_title</a> </li>";
+                    
+                }
+                
+                ?>
+            </ul>
+        </div>
+    </div>
+</div>
 <h1>Welcome New User! <?php echo $_SESSION['user_email']; ?></h1>
 
 
