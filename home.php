335a6cf984c4fce263273109777c8044c5e6059d
isha <ziyaan.verma@gmail.com>
2017-07-29 14:01:06 +0000

    defualt image update

--- home.php
+++ home.php
@@ -8,10 +8,11 @@ session_start();
 <html>
 <head>
 	<title> Welcome user </title>
+	  <link rel="stylesheet" href="css/home_style.css">
 </head>
 
 <body>
-<h1>Welcome <?php echo $_SESSION['user_email']; ?></h1>
+<h1>Welcome New User! <?php echo $_SESSION['user_email']; ?></h1>
 
 
 </body>
