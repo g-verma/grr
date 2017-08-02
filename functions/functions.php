f5c23ca76c016a98a0b9a873d297c819d5a9a72b
isha <ziyaan.verma@gmail.com>
2017-08-02 09:17:17 +0000

    get post function created

--- functions/functions.php
+++ functions/functions.php
@@ -28,6 +28,7 @@ function getTopics(){
 // function to insert posts in database 
 
 function insertPost(){
+    
     if(isset($_POST['sub'])){
         global $con;
         global $user_id;
@@ -50,9 +51,56 @@ function insertPost(){
     }
     
 }
-
-
 //insert function ends here
+
+
+// function for displaying posts
+function get_posts(){
+    global $con;
+    $per_page =5;
+    if(isset($_GET['page']))
+    {
+        $page=$_GET['page'];
+    }
+    else { $page=1;   }
+    
+    $start_from = ($page-1) * $per_page;
+    $get_posts = "select * from posts ORDER by 1 DESC LIMIT $start_from,$per_page";
+    
+    $run_posts =mysqli_query($con,$get_posts);
+    
+    while($row_posts=mysqli_fetch_array($run_posts))
+    {
+        $post_id=$row_posts['post_id'];
+        $user_id=$row_posts['user_id'];
+        $post_title=$row_posts['post_title'];
+        $content=$row_posts['post_content'];
+        $post_date=$row_posts['post_date'];
+    
+        //getting the user who has posted
+        
+        $user="select * from users where user_id='$user_id' AND posts='yes'";
+        $run_user=mysqli_query($con,$user);
+        $row_user=mysqli_fetch_array($run_user);
+        $user_name=$row_user['user_image'];
+        
+        //now displaying all the posts at once
+        
+        echo "<div id='posts'>
+                <p> <img src='user/user_images/$user_image' width='50' height='50'></p>
+                <h3><a href='userprofile.php?user_id=$user_id'>$user_name</a></h3>
+                
+        
+        
+        ";
+        
+    }
+    
+    
+}
+// end of getpost function for displaying posts
+
+
 
 
 ?>
